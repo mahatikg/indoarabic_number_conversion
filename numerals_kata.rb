1f5651cd43b158af8bf42a648577dac840013395
Mahati <mahatigollamudi@gmail.com>
2017-01-09 03:51:46 -0500

    update numerals_kata.rb

--- numerals_kata.rb
+++ numerals_kata.rb
@@ -19,7 +19,7 @@ def conversion_dictionary(indoarabic)
 end
 
 def convert(indoarabic)
-  return Romans couldn't comprehend 0" if indoarabic.zero?
+  return "" if indoarabic.zero?
   arabic_num, roman_num = conversion_dictionary(indoarabic)
   roman_num + convert(indoarabic - arabic_num)
 end
